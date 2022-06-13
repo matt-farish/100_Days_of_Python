fda31af88472b1aa8f01ebd4d0709e2726ec7e8f
matt-farish <mattyfarish@gmail.com>
2022-06-13 20:55:48 +1000

    Update secret_auction.py

--- Day 9/secret_auction.py
+++ Day 9/secret_auction.py
@@ -1,4 +1,4 @@
 # Day nine of Udemy's 100 Days of Python programming course
-
-from replit import clear
+import clear_function
+from clear_function import clear
 #HINT: You can call clear() to clear the output in the console.
