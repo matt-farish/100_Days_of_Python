1ced1bbe3b2ae1cf4295f99973e73caa0303d2e4
matt-farish <mattyfarish@gmail.com>
2022-06-23 13:32:08 +1000

    Completed day 20

--- Day 20/snake_game.py
+++ Day 20/snake_game.py
@@ -1,5 +1,7 @@
 # Day 20 of Udemy's 100 Days of Python programming course
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -7,25 +9,20 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+snake = Snake()
 
-for position in starting_positions:
-    snake_segment = Turtle("square")
-    snake_segment.color("white")
-    snake_segment.penup()
-    snake_segment.goto(position)
-    segments.append(snake_segment)
-
-screen.update()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
 while game_is_on:
-    for segment in segments:
-        segment.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
-
-
+    snake.move()
 
 screen.exitonclick()
