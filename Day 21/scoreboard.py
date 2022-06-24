4d0887d4526f7fdc2f8ce22b860b37a7dd86d46a
Matthew Farish <mattyfarish@gmail.com>
2022-06-24 21:28:15 +1000

    Created new files and completed Day 21 :)

---
+++ Day 21/scoreboard.py
@@ -0,0 +1,27 @@
+# Day 21 of Udemy's 100 Days of Python programming course
+from ctypes import alignment
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align = ALIGNMENT, font = FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over!", align = ALIGNMENT, font = FONT)
