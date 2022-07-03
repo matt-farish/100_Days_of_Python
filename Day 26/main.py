e60089780622c9dbd91f81c126aa7b3a5d0df699
matt-farish <mattyfarish@gmail.com>
2022-07-03 21:44:18 +1000

    Updated with exception handling

--- Day 26/main.py
+++ Day 26/main.py
@@ -6,15 +6,22 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-student_data_frame = pandas.DataFrame(student_dict)
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # Iterate over the rows in the data and create a dictionary using the letter and code in the row.
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
 
-# Answer is equal to the value in the phonetic dictionary, using the letter as the key, for each weather in the word.
-answer = [phonetic_dict[letter] for letter in word]
+    try:
+        # Answer is equal to the value in the phonetic dictionary, using the letter as the key, for each letter in the word.
+        answer = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Please use letters only.")
+        generate_phonetic()
+    else:
+        print(answer)
 
-print(answer)
+
+generate_phonetic()
