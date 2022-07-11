a7aff35c63f622ed75292fac0d5e264c49b77305
Matthew Farish <97074732+matt-farish@users.noreply.github.com>
2022-07-11 14:53:48 +1000

    Actually updated parameter fields >:(

--- Day 37/main.py
+++ Day 37/main.py
@@ -9,8 +9,8 @@ GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "TOKEN",
-    "username": "USERNAME",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
