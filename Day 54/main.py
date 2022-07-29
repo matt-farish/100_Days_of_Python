5429507b1602c28de1424b9aac391952958f2e38
matt-farish <mattyfarish@gmail.com>
2022-07-29 23:04:15 +1000

    Completed day 54

---
+++ Day 54/main.py
@@ -0,0 +1,16 @@
+#Day 54 of Udemy's 100 Days of Python programming course
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+if __name__ == "__main__":
+    app.run()
