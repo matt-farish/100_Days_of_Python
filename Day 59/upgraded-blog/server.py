c8033eebb368d9a75fea38c7f1e013608f50147a
Matthew Farish <mattyfarish@gmail.com>
2022-08-07 18:57:40 +1000

    Completed day 59

--- Day 59/upgraded-blog/server.py
+++ Day 59/upgraded-blog/server.py
@@ -1,13 +1,33 @@
 #Day 59 of Udemy's 100 Days of Python programming course
 from flask import Flask, render_template
+import requests
 
+posts = requests.get("https://api.npoint.io/c39d93cc1c10fbad1137").json()
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
