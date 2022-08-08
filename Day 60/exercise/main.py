6f5512b8a328067c19fd1425ededb1f7b207cb68
Matthew Farish <mattyfarish@gmail.com>
2022-08-08 15:59:24 +1000

    Completed day 60

---
+++ Day 60/exercise/main.py
@@ -0,0 +1,18 @@
+#Day 60 of Udemy's 100 Days of Python programming course
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    name = request.form["name"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}. Password: {password}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
