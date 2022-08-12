2d46004f12fa2ecb2d86bac22db0d2e696fbb54d
Matthew Farish <mattyfarish@gmail.com>
2022-08-12 17:08:28 +1000

    Completed day 64

--- Day 64/main.py
+++ Day 64/main.py
@@ -1,3 +1,4 @@
+#Day 64 of Udemy's 100 Days of Python programming course
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +9,9 @@ import requests
 from decouple import config
 
 API_KEY = config("API_KEY")
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -18,13 +22,15 @@ Bootstrap(app)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), unique = True, nullable = False)
+    title = db.Column(db.String(250), nullable = False)
     year = db.Column(db.Integer, nullable = False)
-    description = db.Column(db.String(200), nullable = False)
-    rating = db.Column(db.Float, nullable = False)
-    ranking = db.Column(db.Integer, nullable = False)
-    review = db.Column(db.String(50), nullable = False)
-    img_url = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(500), nullable = False)
+    rating = db.Column(db.Float, nullable = True)
+    ranking = db.Column(db.Integer, nullable = True)
+    review = db.Column(db.String(250), nullable = True)
+    img_url = db.Column(db.String(250), nullable = False)
+
+db.create_all()
 
 class RateMovieForm(FlaskForm):
     rating = StringField('Your rating (Out of 10)', validators = [DataRequired()])
@@ -35,11 +41,15 @@ class AddMovieForm(FlaskForm):
     title = StringField('Movie title')
     submit = SubmitField("Add movie")
 
-db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html", movies = db.session.query(Movie).all())
+    all_movies = Movie.query.order_by(Movie.rating).all()
+
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+    return render_template("index.html", movies = all_movies)
 
 @app.route("/edit", methods = ["GET", "POST"])
 def edit():
@@ -66,10 +76,29 @@ def delete():
 def add():
     form = AddMovieForm()
     if form.validate_on_submit():
-        title = form.title.data
-        print(title)
-        return redirect(url_for('add'))
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params = {"api_key": API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        print(data)
+        return render_template("select.html", options = data)
     return render_template("add.html", form = form)
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get('id')
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params = {"api_key": API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title = data["title"],
+            year = data["release_date"].split("-")[0],
+            img_url = f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description = data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("edit", id = new_movie.id))
 
 if __name__ == '__main__':
     app.run(debug=True)
